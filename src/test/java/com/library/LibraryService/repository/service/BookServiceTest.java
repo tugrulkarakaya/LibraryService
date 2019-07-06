a083a5e9e1c352acadd55edda925f3a8b9be10af
tugrul <tugrul@campaignup.co.uk>
2019-07-06 15:40:14 +0100

    Service UT is being developed

---
+++ src/test/java/com/library/LibraryService/repository/service/BookServiceTest.java
@@ -0,0 +1,87 @@
+package com.library.LibraryService.repository.service;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import com.library.LibraryService.model.Book;
+import com.library.LibraryService.payload.book.GetAllBooksRequest;
+import com.library.LibraryService.repository.BookRepository;
+import com.library.LibraryService.service.BookService;
+import com.library.LibraryService.service.BookServiceImp;
+
+
+public class BookServiceTest {
+
+	@TestConfiguration
+    static class BookServiceImplTestContextConfiguration { 
+        @Bean
+        public BookService pollService() {
+            return new BookServiceImp();
+        }
+    }
+	
+	@Autowired
+	private BookService bookService;
+	
+	@MockBean
+	BookRepository bookRepository;
+	
+	@Before
+	public void setUp() {
+		Book book1 = new Book("Book Name1", "Book Author1", LocalDate.of(2011, 12, 15), new BigDecimal(40));
+		book1.setId(1L);
+		Book book2 = new Book("Book Name2", "Book Author1", LocalDate.of(2011, 12, 16), new BigDecimal(50));
+		book2.setId(2L);
+		Book book3 = new Book("Book Name3", "Book Author1", LocalDate.of(2011, 12, 17), new BigDecimal(60));
+		book3.setId(3L);
+		Book book4 = new Book("Book Name4", "Book Author2", LocalDate.of(2011, 12, 18), new BigDecimal(70));
+		book4.setId(4L);
+		Book book5 = new Book("Book Name5", "Book Author2", LocalDate.of(2011, 12, 19), new BigDecimal(80));
+		book5.setId(5L);
+			
+	    Mockito.when(bookRepository.findById(book1.getId())).thenReturn(Optional.of(book1));
+	    Mockito.when(bookRepository.findById(book2.getId())).thenReturn(Optional.of(book2));
+	    Mockito.when(bookRepository.findById(book3.getId())).thenReturn(Optional.of(book3));
+	    Mockito.when(bookRepository.findById(book4.getId())).thenReturn(Optional.of(book4));
+	    Mockito.when(bookRepository.findById(book5.getId())).thenReturn(Optional.of(book5));
+	    
+	    List<Book> books1 =  new ArrayList<Book>();
+	    books1.add(book1);books1.add(book2); books1.add(book3);	  
+	    List<Book> books2 =  new ArrayList<Book>();
+	    books2.add(book4);books2.add(book5);	  
+	    Page<Book> pageBook = new PageImpl<Book>(books1);	    
+	    Mockito.when(bookRepository.findByAuthor("Book Author1", null)).thenReturn(pageBook);
+	    
+	    
+	    GetAllBooksRequest bookRequest = new GetAllBooksRequest();
+	    bookRequest.setFilter("Book");
+	    List<Book> books3 = new ArrayList<>();
+	    books3.addAll(books1); books3.addAll(books2);
+	    Mockito.when(bookRepository.findAllBooksWithPagination(bookRequest, null)).thenReturn(books3);
+	    
+	    
+	}	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
