855617a12959b36f309e14dcca8a5e9a257b45c4
tugrul <tugrul@campaignup.co.uk>
2019-07-06 21:06:20 +0100

    avoid error swagger controllers

--- src/main/java/com/library/LibraryService/configuration/SpringFoxConfig.java
+++ src/main/java/com/library/LibraryService/configuration/SpringFoxConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -43,7 +45,7 @@ public class SpringFoxConfig {
                 .apiInfo(apiInfo())
                 .select()
                     .apis(RequestHandlerSelectors.any())
-                    .paths(PathSelectors.any())
+                    .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
     }
 	
